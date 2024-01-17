4bc1856087f81a6c82470604628f342322e465d2
Matthias Nannt <mail@matthiasnannt.com>
2024-01-17 15:50:41 +0100

    add ui-shadcn

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans", });
 
 export const metadata: Metadata = {
   title: "oss.gg",
