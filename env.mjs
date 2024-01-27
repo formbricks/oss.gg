a974c1b97f9af697702871ffd4651b8212f75846
Matthias Nannt <mail@matthiasnannt.com>
2024-01-27 19:08:28 +0100

    make github access token optional

--- env.mjs
+++ env.mjs
@@ -9,7 +9,7 @@ export const env = createEnv({
     NEXTAUTH_SECRET: z.string().min(1),
     GITHUB_CLIENT_ID: z.string().min(1),
     GITHUB_CLIENT_SECRET: z.string().min(1),
-    GITHUB_ACCESS_TOKEN: z.string().min(1),
+    GITHUB_ACCESS_TOKEN: z.string().min(1).optional(),
     DATABASE_URL: z.string().min(1),
     SMTP_FROM: z.string().min(1),
     SMTP_HOST: z.string().min(1),
