9021341da49b76e8a357128d295437478087dfa8
ShubhamPalriwala <spalriwalau@gmail.com>
2024-03-05 13:13:01 +0530

    debug: enrollment action

--- app/(dashboard)/enroll/[repositoryId]/actions.ts
+++ app/(dashboard)/enroll/[repositoryId]/actions.ts
@@ -17,6 +17,7 @@ export const enrollCurrentUserAction = async (repositoryId: string) => {
     }
 
     const fullEnrollmentData = { repositoryId: repositoryId, userId: user.id };
+    console.log("full enroll data", fullEnrollmentData);
 
     const enrollment = await createEnrollment(fullEnrollmentData);
     return enrollment;
