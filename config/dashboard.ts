cd7aa6a3cbfa9e053532362c7ac59a2711ca69f9
Chigala <92148630+Chigala@users.noreply.github.com>
2024-03-21 16:18:26 +0100

    added enroll player component

--- config/dashboard.ts
+++ config/dashboard.ts
@@ -1,10 +1,11 @@
 import { DashboardConfig } from "types";
 
-
 export const dashboardConfig: DashboardConfig = {
   mainNav: [
+    { title: "Home", href: "/dashboard" },
     { title: "Open issues", href: "/issues" },
     { title: "Enroll to play", href: "/enroll" },
+    { title: "Settings", href: "/settings" },
   ],
   bottomNav: [
     { title: "What is oss.gg?", href: "/", external: true },
