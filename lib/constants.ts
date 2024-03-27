a754afaefdec6181f8c9ec7e77dadd0d92875c8c
Chigala <92148630+Chigala@users.noreply.github.com>
2024-03-27 09:30:36 +0100

    added image upload functionality

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,6 @@
 import { env } from "../env.mjs";
 
+
 export const DEFAULT_CACHE_REVALIDATION_INTERVAL = 60 * 30; // 30 minutes
 
 export const GITHUB_CACHE_REVALIDATION_INTERVAL = 60 * 60 * 24; // 24 hours
@@ -29,3 +30,15 @@ export const GITHUB_APP_SLUG = env.GITHUB_APP_SLUG as string;
 export const GITHUB_APP_ACCESS_TOKEN = env.GITHUB_APP_ACCESS_TOKEN as string;
 
 export const OSS_GG_LABEL = "🕹️ oss.gg" as const;
+
+// Storage constants
+export const S3_ACCESS_KEY = env.S3_ACCESS_KEY;
+export const S3_SECRET_KEY = env.S3_SECRET_KEY;
+export const S3_REGION = env.S3_REGION;
+export const S3_ENDPOINT_URL = env.S3_ENDPOINT_URL;
+export const S3_BUCKET_NAME = env.S3_BUCKET_NAME;
+export const MAX_SIZE = 1024 * 1024 * 10 
+
+// URLs
+export const WEBAPP_URL =
+  env.WEBAPP_URL || (env.VERCEL_URL ? `https://${env.VERCEL_URL}` : false) || "http://localhost:3000";
