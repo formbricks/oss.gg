29f42e8494e06d60b0aa723a1861dbb1bb1b62bf
Kritik Jiyaviya <kritikjiyaviya07@gmail.com>
2024-03-27 16:33:13 +0530

    feat(ui): page-tabs component

---
+++ components/ui/page-tabs.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import * as React from "react";
+
+import { Button } from "./button";
+
+export type PageTabsProps = {
+  tabs: {
+    label: string;
+    href: string;
+  }[];
+  className?: string;
+};
+
+export function PageTabs({ tabs, className }: PageTabsProps) {
+  const pathName = usePathname();
+
+  return (
+    <div className={cn(`flex flex-row rounded-lg bg-muted p-1`, className)}>
+      {tabs.map((tab, index) => (
+        <Button
+          asChild
+          size="sm"
+          variant="secondary"
+          className={cn(
+            pathName === tab.href ? "bg-background text-primary hover:bg-background" : "text-muted-foreground"
+          )}>
+          <Link key={index} href={tab.href} className={cn("group flex flex-col items-center")}>
+            {tab.label}
+          </Link>
+        </Button>
+      ))}
+    </div>
+  );
+}
