8c1bc9c9303fde15f6c3e8bfd16eedc1ec0a826a
Johannes <johannes@formbricks.com>
2024-03-29 16:57:29 +0100

    add settings page, clean up, adjust delete action

--- components/delete-account-card.tsx
+++ components/delete-account-card.tsx
@@ -2,15 +2,14 @@
 
 import { useDeleteAccountModal } from "@/components/delete-modal";
 import { Button } from "@/components/ui/button";
-import { Delete } from "lucide-react";
 
 export default function DeleteAccountCard() {
   const { setShowDeleteAccountModal, DeleteAccountModal } = useDeleteAccountModal();
   return (
     <div>
       <DeleteAccountModal />
-      <div className="h-fit w-full space-y-3 rounded-lg bg-zinc-50 p-5 ">
-        <p className="text-md font-bold">Account </p>
+      <div className="h-fit w-full space-y-3 rounded-lg bg-muted p-5 ">
+        <p className="text-md">Danger Zone </p>
         <Button
           onClick={() => {
             setShowDeleteAccountModal(true);
