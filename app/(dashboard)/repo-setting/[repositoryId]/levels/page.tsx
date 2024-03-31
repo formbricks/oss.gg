15d152624a4c5275a150b92cafa9a3d999ebaeaa
Chigala <92148630+Chigala@users.noreply.github.com>
2024-03-31 22:46:07 +0100

    added dummydata and a level-form-container component

--- app/(dashboard)/repo-setting/[repositoryId]/levels/page.tsx
+++ app/(dashboard)/repo-setting/[repositoryId]/levels/page.tsx
@@ -1,4 +1,5 @@
-import { LevelsForm } from "@/components/forms/levels-form";
+import { LevelsForm, LevelsFormProps } from "@/components/forms/levels-form";
+import LevelsFormContainer from "@/components/level-form-container";
 import LevelsTagInput from "@/components/levels-tag-input";
 
 export const metadata = {
@@ -6,10 +7,108 @@ export const metadata = {
   description: " Set up levels for your repository.",
 };
 
+const LevelDummyData: LevelsFormProps[] = [
+  {
+    levelName: "Level 1",
+    description: "Description for level 1",
+    iconUrl: "https://via.placeholder.com/150",
+    canHuntBounties: true,
+    canReportBugs: true,
+    limitIssues: true,
+    topics: [
+      {
+        id: "1",
+        text: "topic1",
+      },
+      {
+        id: "2",
+        text: "topic2",
+      },
+    ],
+    pointThreshold: 200,
+  },
+  {
+    levelName: "Level 2",
+    description: "Description for level 2",
+    iconUrl: "https://via.placeholder.com/150",
+    canHuntBounties: true,
+    canReportBugs: false,
+    limitIssues: false,
+    topics: [
+      {
+        id: "3",
+        text: "topic3",
+      },
+      {
+        id: "4",
+        text: "topic4",
+      },
+    ],
+    pointThreshold: 500,
+  },
+  {
+    levelName: "Level 3",
+    description: "Description for level 3",
+    iconUrl: "https://via.placeholder.com/150",
+    canHuntBounties: false,
+    canReportBugs: true,
+    limitIssues: true,
+    topics: [
+      {
+        id: "5",
+        text: "topic5",
+      },
+      {
+        id: "6",
+        text: "topic6",
+      },
+    ],
+    pointThreshold: 1000,
+  },
+  {
+    levelName: "Level 4",
+    description: "Description for level 4",
+    iconUrl: "https://via.placeholder.com/150",
+    canHuntBounties: true,
+    canReportBugs: true,
+    limitIssues: false,
+    topics: [
+      {
+        id: "7",
+        text: "topic7",
+      },
+      {
+        id: "8",
+        text: "topic8",
+      },
+    ],
+    pointThreshold: 2000,
+  },
+  {
+    levelName: "Level 5",
+    description: "Description for level 5",
+    iconUrl: "https://via.placeholder.com/150",
+    canHuntBounties: false,
+    canReportBugs: false,
+    limitIssues: true,
+    topics: [
+      {
+        id: "9",
+        text: "topic9",
+      },
+      {
+        id: "10",
+        text: "topic10",
+      },
+    ],
+    pointThreshold: 5000,
+  },
+];
+
 export default function Levels() {
   return (
     <div>
-      <LevelsForm />
+      <LevelsFormContainer data={LevelDummyData} />
     </div>
   );
 }
---
+++ components/level-form-container.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import React from "react";
+
+import { LevelsForm, LevelsFormProps } from "./forms/levels-form";
+import { Button } from "./ui/button";
+
+export default function LevelsFormContainer({ data }: { data: LevelsFormProps[] }) {
+  const [showForm, setShowForm] = React.useState(false);
+  return (
+    <div className="space-y-6">
+      {data.map((level, index) => (
+        <LevelsForm
+          canHuntBounties={level.canHuntBounties}
+          canReportBugs={level.canReportBugs}
+          description={level.description}
+          iconUrl={level.iconUrl}
+          key={index}
+          levelName={level.levelName}
+          limitIssues={level.limitIssues}
+          topics={level.topics}
+          pointThreshold={level.pointThreshold}
+        />
+      ))}
+
+      {showForm && <LevelsForm isForm={true} />}
+
+      <div>
+        <Button variant="secondary" onClick={() => setShowForm(!showForm)}>
+          Add Level
+        </Button>
+      </div>
+    </div>
+  );
+}
