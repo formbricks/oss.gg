5b17e03bb7d170ef5a849a7d6e9e36226be0a120
manish-singh-bisht <mthefool218@gmail.com>
2024-04-02 13:39:54 +0530

    corrected the condition

--- app/(dashboard)/enroll/[repositoryId]/leaderboard/page.tsx
+++ app/(dashboard)/enroll/[repositoryId]/leaderboard/page.tsx
@@ -14,12 +14,15 @@ export default async function LeaderboardPage({ params }) {
 
   return (
     <>
-      {leaderboardProfiles.length === 0 && <p>No users found in the leaderboard.</p>}
-      <Leaderboard
-        leaderboardProfiles={leaderboardProfiles}
-        itemPerPage={ITEMS_PER_PAGE}
-        repositoryId={repository.id}
-      />
+      {leaderboardProfiles.length === 0 ? (
+        <p>No users found in the leaderboard.</p>
+      ) : (
+        <Leaderboard
+          leaderboardProfiles={leaderboardProfiles}
+          itemPerPage={ITEMS_PER_PAGE}
+          repositoryId={repository.id}
+        />
+      )}
     </>
   );
 }
