898f4d8afbd8f843ed572c448f93ae8f86959440
manish-singh-bisht <mthefool218@gmail.com>
2024-04-04 00:04:54 +0530

    tweaks

--- components/ui/pointsCard.tsx
+++ components/ui/pointsCard.tsx
@@ -18,7 +18,7 @@ const PointsCard: React.FC<PointsCardProps> = ({
   return (
     <>
       <div className="flex h-72 justify-center rounded-lg bg-secondary">
-        <div className="m-6 flex w-full flex-col items-center gap-4  rounded-lg bg-popover p-4 font-semibold">
+        <div className="m-6 flex w-full flex-col items-center gap-4  rounded-lg bg-popover p-6 font-semibold">
           <div className="flex items-center gap-2">
             <div className="text-3xl">{repositoryName}</div>
             {repositoryLogo && (
