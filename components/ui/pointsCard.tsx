4ad7dd26052d1f23ebf93c990392b3e8edb6af51
manish-singh-bisht <mthefool218@gmail.com>
2024-04-07 00:39:24 +0530

    tweaks

--- components/ui/pointsCard.tsx
+++ components/ui/pointsCard.tsx
@@ -15,10 +15,18 @@ const PointsCard = ({ repositoryName, repositoryLogo, points, rank }: PointsCard
       <div className="flex items-center justify-center rounded-lg bg-secondary">
         <div className="m-6 flex w-full flex-col items-center   rounded-lg bg-popover p-4 font-semibold">
           <div className="flex items-center gap-2">
-            <div className="text-3xl">{repositoryName}</div>
-            {repositoryLogo && <Image src={repositoryLogo} height={40} width={40} alt="repository-logo" className="rounded-lg" />}
+            <div className="text-2xl lg:text-3xl">{repositoryName}</div>
+            {repositoryLogo && (
+              <Image
+                src={repositoryLogo}
+                height={40}
+                width={40}
+                alt="repository-logo"
+                className="rounded-lg"
+              />
+            )}
           </div>
-          <div className="text-8xl font-bold">{points}</div>
+          <div className="text-5xl font-bold lg:text-8xl">{points}</div>
           <div>{rank === null ? "Earn points to see rankings" : `# Rank ${rank}`}</div>
         </div>
       </div>
