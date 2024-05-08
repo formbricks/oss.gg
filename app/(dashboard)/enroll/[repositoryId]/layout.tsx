df41c6d0860a6e6f6fcdce321e5d8810e6532c7b
Manish Singh Bisht <114493480+manish-singh-bisht@users.noreply.github.com>
2024-05-08 19:37:21 +0530

    added dynamic metadata (#80)

--- app/(dashboard)/enroll/[repositoryId]/layout.tsx
+++ app/(dashboard)/enroll/[repositoryId]/layout.tsx
@@ -1,11 +1,20 @@
 import { getRepositoryById } from "@/lib/repository/service";
+import type { Metadata } from "next";
 
 import LayoutTabs from "./layoutTabs";
 
-export const metadata = {
-  title: "Formbricks",
-  description: "Contribute to the worlds fastest growing survey infrastructure.",
-};
+interface MetadataProps {
+  params: { repositoryId: string };
+}
+
+export async function generateMetadata({ params }: MetadataProps): Promise<Metadata> {
+  const repository = await getRepositoryById(params.repositoryId);
+
+  return {
+    title: repository?.name || "",
+    description: repository?.description || "",
+  };
+}
 
 export default async function RepoDetailPageLayout({ params, children }) {
   const repository = await getRepositoryById(params.repositoryId);
