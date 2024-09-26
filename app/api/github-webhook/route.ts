c4c59aa5fc41a970fd3474177920f17c57439dcd
Dhruwang <dhruwangjariwala18@gmail.com>
2024-09-26 15:22:54 +0530

    removed octakit from app dir

--- app/api/github-webhook/route.ts
+++ app/api/github-webhook/route.ts
@@ -1,5 +1,4 @@
 import { registerHooks } from "@/lib/github";
-import { EmitterWebhookEvent, EmitterWebhookEventName } from "@octokit/webhooks";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
@@ -8,7 +7,7 @@ export async function POST(req: Request) {
   const eventId = headersList.get("x-github-delivery") as string;
   const githubEvent = headersList.get("x-github-event") as string;
 
-  let body: EmitterWebhookEvent<"issue_comment" | "pull_request" | "installation">["payload"];
+  let body: any;
 
   try {
     body = await req.json();
@@ -20,7 +19,7 @@ export async function POST(req: Request) {
     return NextResponse.json({ error: "Missing X-GitHub-Delivery header" }, { status: 400 });
   }
 
-  registerHooks(githubEvent as EmitterWebhookEventName, body);
+  registerHooks(githubEvent, body);
 
   return NextResponse.json({ message: `Event ${eventId} processed` }, { status: 200 });
 }
--- lib/github/index.ts
+++ lib/github/index.ts
@@ -19,7 +19,7 @@ import {
 } from "./hooks/issue";
 
 export const registerHooks = async (
-  event: EmitterWebhookEventName,
+  event: string,
   body: EmitterWebhookEvent<"issue_comment.created" | "pull_request" | "installation" | "issues">["payload"]
 ) => {
   switch (event) {
