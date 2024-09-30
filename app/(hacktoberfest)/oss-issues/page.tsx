07235063359368bc52023d67649d7af09b8ee85b
Johannes <johannes@formbricks.com>
2024-09-30 13:46:44 -0700

    update ossissue page

--- app/(hacktoberfest)/oss-issues/page.tsx
+++ app/(hacktoberfest)/oss-issues/page.tsx
@@ -1,24 +1,50 @@
-import { getAllOssGgIssuesOfRepos } from "@/lib/github/service";
-import { getAllRepositories } from "@/lib/repository/service";
-import { TPullRequest } from "@/types/pullRequest";
 import Link from "next/link";
 
 export default async function IssuesPage() {
-  const ossGgRepositories = await getAllRepositories();
+  /*   const ossGgRepositories = await getAllRepositories();
   const pullRequests: TPullRequest[] = await getAllOssGgIssuesOfRepos(
     ossGgRepositories.map((repo) => ({ id: repo.githubId, fullName: `${repo.owner}/${repo.name}` }))
-  );
+  ); */
+
+  const issueLists = [
+    {
+      repo: "dub",
+      href: "https://github.com/dubinc/dub/issues?q=is:open+is:issue+label:%22%F0%9F%95%B9%EF%B8%8F+oss.gg%22",
+    },
+    {
+      repo: "formbricks",
+      href: "https://github.com/formbricks/formbricks/issues?q=is:open+is:issue+label:%22%F0%9F%95%B9%EF%B8%8F+oss.gg%22",
+    },
+    {
+      repo: "hanko",
+      href: "https://github.com/teamhanko/hanko/issues?q=is:open+is:issue+label:%22%F0%9F%95%B9%EF%B8%8F+oss.gg%22",
+    },
+    {
+      repo: "openbb",
+      href: "https://github.com/OpenBB-finance/OpenBB/issues?q=is:open+is:issue+label:%22%F0%9F%95%B9%EF%B8%8F+oss.gg%22",
+    },
+    {
+      repo: "papermark",
+      href: "https://github.com/mfts/papermark/issues?q=is:open+is:issue+label:%22%F0%9F%95%B9%EF%B8%8F+oss.gg%22",
+    },
+    {
+      repo: "twenty",
+      href: "https://github.com/twentyhq/twenty/issues?q=is:open+is:issue+label:%22%F0%9F%95%B9%EF%B8%8F+oss.gg%22",
+    },
+    {
+      repo: "unkey",
+      href: "https://github.com/unkeyed/unkey/issues?q=is:open+is:issue+label:%22%F0%9F%95%B9%EF%B8%8F+oss.gg%22",
+    },
+  ];
 
   return (
     <div className="space-y-2 font-mono text-xs">
-      <h1 className="pb-2 font-bold">available issues ({pullRequests.length})</h1>
+      <h1 className="pb-2 font-bold">available issues</h1>
       <ul className="list-none space-y-2">
-        {pullRequests.map((pullRequest) => (
+        {issueLists.map((pullRequest) => (
           <li key={pullRequest.href}>
-            <Link href={pullRequest.href} className="underline-offset-4 hover:underline">
-              {pullRequest.repositoryFullName && <span>{pullRequest.repositoryFullName}</span>}
-              {pullRequest.points !== undefined && <span> | {pullRequest.points} points 🕹️ | </span>}
-              <span>{pullRequest.title}</span>
+            <Link href={pullRequest.href} className="underline-offset-4 hover:underline" target="_blank">
+              <span>{pullRequest.repo}</span>
             </Link>
           </li>
         ))}
