a5dbd5b057a7b2811c1f21b48feac2e844fd23a4
Dhruwang <dhruwangjariwala18@gmail.com>
2024-09-30 17:02:15 +0530

    removed processed events

--- app/api/github-webhook/route.ts
+++ app/api/github-webhook/route.ts
@@ -3,8 +3,6 @@ import { EmitterWebhookEvent, EmitterWebhookEventName } from "@octokit/webhooks"
 import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
-// Set to store processed event IDs
-const processedEvents = new Set<string>();
 export async function POST(req: NextRequest) {
   const headersList = headers();
   const eventId = headersList.get("x-github-delivery") as string;
@@ -22,19 +20,8 @@ export async function POST(req: NextRequest) {
     return NextResponse.json({ error: "Missing X-GitHub-Delivery header" }, { status: 400 });
   }
 
-  if (processedEvents.has(eventId)) {
-    return NextResponse.json({ message: `Event ${eventId} already processed, skipping` }, { status: 200 });
-  }
   console.log("registerHooks");
   registerHooks(githubEvent as EmitterWebhookEventName, body);
-
-  processedEvents.add(eventId);
-  setTimeout(
-    () => {
-      processedEvents.delete(eventId);
-    },
-    24 * 60 * 60 * 1000
-  ); // 24 hours
 
   return NextResponse.json({ message: `Event ${eventId} processed` }, { status: 200 });
 }
