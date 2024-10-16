26cd9f77e09f1fe0887625d93f872036a826de49
Steven Tey <stevensteel97@gmail.com>
2024-10-16 14:10:15 -0700

    Add ISR for leaderboard page

--- app/(hacktoberfest)/leaderboard/page.tsx
+++ app/(hacktoberfest)/leaderboard/page.tsx
@@ -2,6 +2,9 @@ import { getAllUserPointsList } from "@/lib/points/service";
 
 import LeaderBoard from "./components";
 
+// revalidate once every hour
+export const revalidate = 3600
+
 export default async function Page() {
   const allUserPoints = await getAllUserPointsList();
   const filteredUserPoints = allUserPoints.filter((user) => user.totalPoints > 0);
